a4df0100924db8ba22b258b876f0c625a6623a0d
chinnam-arjun <chinnammallikarjunarao.99@gmail.com>
2026-05-09 22:13:26 +0530

    added axios interceptors res, err to handle cross platform ( chrome, edge etc) cors req handlings about token

--- src/utils/AxiosInstance.js
+++ src/utils/AxiosInstance.js
@@ -9,6 +9,7 @@ const axiosInstance = axios.create({
     withCredentials: false
 });
 
+// 1. Request Interceptor (Adds the token)
 axiosInstance.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem("token");
@@ -22,4 +23,48 @@ axiosInstance.interceptors.request.use(
     }
 );
 
+// 2. Response Interceptor (Handles the 401 Unauthorized)
+axiosInstance.interceptors.response.use(
+    (response) => {
+        return response; // Success! Just pass the response through
+    },
+    (error) => {
+        // This is where the 401 is caught
+        if (error.response && error.response.status === 401) {
+            console.log("Unauthorized! Redirecting...");
+            localStorage.removeItem('token');
+            window.location.href = '/signin';
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default axiosInstance;
+
+// import axios from "axios";
+// import { baseUrl } from "./base_url";
+
+// const axiosInstance = axios.create({
+//     baseURL: baseUrl,
+//     headers: {
+//         "Content-Type": "application/json",
+//     },
+//     withCredentials: false
+// });
+
+// axiosInstance.interceptors.request.use(
+//     (config) => {
+//         const token = localStorage.getItem("token");
+//         if (token) {
+//             config.headers["Authorization"] = `Bearer ${token}`;
+//         }
+//         return config;
+//     },
+//     (error) => {
+        
+//     return Promise.reject(error);
+//     }
+
+// );
+
+// export default axiosInstance;
