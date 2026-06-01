c45ce237bb0c053d7f47251962e9eb3d5b8710cf
chinnam-arjun <chinnammallikarjunarao.99@gmail.com>
2026-06-01 14:25:00 +0530

    updated main.jsx -> removed strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Router>
@@ -29,5 +29,5 @@ createRoot(document.getElementById('root')).render(
         </Router>
       </PersistGate>
     </Provider>
-  </StrictMode>,
+  
 )
