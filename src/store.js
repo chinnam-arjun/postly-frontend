a4c42b5785649cf85d3f5f8d7685329796d7674c
chinnam-arjun <chinnammallikarjunarao.99@gmail.com>
2026-06-01 13:39:05 +0530

    added article reducer in store.js

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./redux_slices/authSlice";
 import postsReducer from "./redux_slices/postSlice";
 import userReducer from "./redux_slices/userSlice";
+import articleReducer from "./redux_slices/articleSlice";
 
 import {
     persistStore,
@@ -19,7 +20,8 @@ import storage from "redux-persist/lib/storage";
 const rootReducer = combineReducers({
     auth: authReducer, 
     posts: postsReducer,
-    users: userReducer
+    users: userReducer,
+    articles: articleReducer,
 });
 
 const persistConfig = {
